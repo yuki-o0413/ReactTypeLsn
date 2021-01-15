e4ebdb63f1f092398a0b5118b423ffb94ccc3ec7
yoji <ojimay@colabmix.co.jp>
2021-01-16 00:42:27 +0900

    with error

--- src/App.tsx
+++ src/App.tsx
@@ -6,30 +6,11 @@ import Content from './components/Content';
 
 
 function App() {
-  onChangeId = (e) => {
-    console.log(e.target.value)
-    this.props.updateFormId(e.target.value);
-  }
-  // forName
-  onChangeName = (e) => {
-      console.log(e.target.value)
-      this.props.updateFormName(e.target.value);
-  }
-  // clickSaveButton
-  onSaveItem = () => {
-      console.log(this.props)
-      console.log("SAVE_ID: ",this.props.id);
-      console.log("SAVE_NAME: ",this.props.name);
-  }
 
-  const formItem = this.props
-  // formEvent
-  const contentHandler = ({onChangeId, onChangeName, onSaveItem}) => ({ onChangeId,   onChangeName, onSaveItem })
-    console.log('App.render:', this.props);
 
   return (
     <>
-      <Content formItem={formItem} {...contentHandler(this)} />
+      <Content />
     </>
   );
 }
