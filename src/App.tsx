374ff35a4594adc047aade25691c4eddaa4143bb
yoji <ojimay@colabmix.co.jp>
2021-01-16 00:14:44 +0900

    with error

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
+
 import './App.css';
+import Content from './components/Content';
+
 
 function App() {
+  onChangeId = (e) => {
+    console.log(e.target.value)
+    this.props.updateFormId(e.target.value);
+  }
+  // forName
+  onChangeName = (e) => {
+      console.log(e.target.value)
+      this.props.updateFormName(e.target.value);
+  }
+  // clickSaveButton
+  onSaveItem = () => {
+      console.log(this.props)
+      console.log("SAVE_ID: ",this.props.id);
+      console.log("SAVE_NAME: ",this.props.name);
+  }
+
+  const formItem = this.props
+  // formEvent
+  const contentHandler = ({onChangeId, onChangeName, onSaveItem}) => ({ onChangeId,   onChangeName, onSaveItem })
+    console.log('App.render:', this.props);
+
   return (
     <>
-    
+      <Content />
     </>
   );
 }
---
+++ src/components/Content.tsx
@@ -0,0 +1,62 @@
+'use strict';
+import React,{ useState } from 'react';
+import { createStore } from "redux";
+
+export default function Content(props) {
+  //NAME３文字以上ででsaveボタン押せるようにdisabledを入れる
+  const [checkName, setCheckName] = useState(true);
+  // const ids = props.formItem.id;
+  // const names = props.formItem.name;
+  // console.log(props.formItem.id["list"])
+  // const ids = props.formItem.id;
+  console.log(props)
+  return (
+    <>
+      <p>content</p>
+​
+      <Form
+        onChangeId={props.onChangeId}
+        onChangeName={props.onChangeName}
+        setCheckName={setCheckName}
+      />
+      <button type="button"
+        className="btn btn-outline-success"
+        onClick={(e) => {props.onSaveItem();console.log(e)}}
+        disabled = {checkName}
+        >
+        SAVE
+      </button>
+    </>
+  );
+}
+
+function Form(props) {
+  return (
+    <form>
+      <div className="form-group">
+        <label className="pt-2">ID</label>
+        <input id="id"
+          type="text"
+          className="form-control"
+          value={props.id}
+          onChange={(e) => props.onChangeId(e)}
+        />
+        <label className="pt-2">Name</label>
+        <input id="name"
+          type="text"
+          className="form-control"
+          value={props.name}
+          onChange={
+            (e) =>
+            {
+              if (e.target.value.length >= 3){
+                setCheckName(false)
+                {props.onChangeName(e)}
+              }
+            }
+          }
+        />
+      </div>
+    </form>
+  )
+}
