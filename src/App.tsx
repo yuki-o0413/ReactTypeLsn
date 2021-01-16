3995335d6751598253aa79707cb594c4575dc82d
yoji <ojimay@colabmix.co.jp>
2021-01-16 19:38:54 +0900

    added old file to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,31 @@ import logo from './logo.svg';
 import './App.css';
 import Content from './components/Content';
 
-
 function App() {
+  onChangeId = (e) => {
+    console.log(e.target.value)
+    this.props.updateFormId(e.target.value);
+  }
+// forName
+  onChangeName = (e) => {
+    console.log(e.target.value)
+    this.props.updateFormName(e.target.value);
+  }
+// clickSaveButton
+  onSaveItem = () => {
+    console.log(this.props)
+    console.log("SAVE_ID: ",this.props.id);
+    console.log("SAVE_NAME: ",this.props.name);
+  }
 
+  const formItem = this.props
+    // formEvent
+    const contentHandler = ({onChangeId, onChangeName, onSaveItem}) => ({ onChangeIdonChangeName, onSaveItem })
+    console.log('App.render:', this.props);
 
   return (
     <>
-      <Content />
+      <Content formItem={formItem} {...contentHandler(this)} />
     </>
   );
 }
